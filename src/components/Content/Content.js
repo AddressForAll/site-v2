d600be934d1691009c2c82b5d125f91de34d6808
elpbatista <elpbatista@gmail.com>
2022-03-19 00:13:54 -0700

    changing class to className

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -21,9 +21,9 @@ const Content = () => (
                         <iframe
                         src="https://www.youtube.com/embed/6sT6Xm8jwCI"
                         title="YouTube video player"
-                        frameborder="0"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
+                        allowFullScreen
                         ></iframe>
                     </div>
                 </RowContainer>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,68 +2,74 @@ import React from 'react'
 import Container from '../Container/Container'
 
 const Footer = () => (
-    <footer>
-        <Container>
-            <div class="row">
-            <div class="col col-1-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                ducimus sunt sapiente dolorum doloremque voluptatibus delectus modi
-                aliquam quas dolor tempore nisi, est, quibusdam natus. Est illo
-                quisquam cumque mollitia?
-            </div>
-            <div class="col col-1-5">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
-                veritatis animi nemo provident iure culpa natus molestias est.
-                Temporibus consequuntur vero similique facere quam libero officiis
-                pariatur repudiandae tenetur nisi.
-            </div>
-            <div class="col col-1-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                praesentium, quae ea excepturi id libero, enim maiores, quo error
-                fugit ducimus! Corrupti ducimus qui labore dolorum voluptatum.
-                Vitae, sequi perferendis?
-            </div>
-            <div class="col col-2-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga saepe
-                suscipit qui dolorum, blanditiis, mollitia repellat cupiditate
-                inventore, quasi ullam atque obcaecati modi odio perferendis
-                temporibus eos facilis? Qui, accusamus.
-            </div>
-            </div>
-            <hr />
-            <div class="row">
-            <div class="col col-4-5"><p>© 2022 AddressForAll</p></div>
-            <div class="col col-1-5">
-                <ul id="sn-icons" class="nav">
-                <li>
-                    <a href="#"
-                    ><svg>
-                        <use xlinkHref="#github"></use></svg
-                    ></a>
-                </li>
-                <li>
-                    <a href="#"
-                    ><svg>
-                        <use xlinkHref="#twitter"></use></svg
-                    ></a>
-                </li>
-                <li>
-                    <a href="#"
-                    ><svg>
-                        <use xlinkHref="#instagram"></use></svg
-                    ></a>
-                </li>
-                <li>
-                    <a href="#"
-                    ><svg>
-                        <use xlinkHref="#facebook"></use></svg
-                    ></a>
-                </li>
-                </ul>
-            </div>
-            </div>
-        </Container>
-    </footer>
-)
+  <footer>
+    <Container>
+      <div className="row">
+        <div className="col col-1-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          ducimus sunt sapiente dolorum doloremque voluptatibus delectus modi
+          aliquam quas dolor tempore nisi, est, quibusdam natus. Est illo
+          quisquam cumque mollitia?
+        </div>
+        <div className="col col-1-5">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
+          veritatis animi nemo provident iure culpa natus molestias est.
+          Temporibus consequuntur vero similique facere quam libero officiis
+          pariatur repudiandae tenetur nisi.
+        </div>
+        <div className="col col-1-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          praesentium, quae ea excepturi id libero, enim maiores, quo error
+          fugit ducimus! Corrupti ducimus qui labore dolorum voluptatum. Vitae,
+          sequi perferendis?
+        </div>
+        <div className="col col-2-5">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga saepe
+          suscipit qui dolorum, blanditiis, mollitia repellat cupiditate
+          inventore, quasi ullam atque obcaecati modi odio perferendis
+          temporibus eos facilis? Qui, accusamus.
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col col-4-5">
+          <p>© 2022 AddressForAll</p>
+        </div>
+        <div className="col col-1-5">
+          <ul id="sn-icons" className="nav">
+            <li>
+              <a href="#">
+                <svg>
+                  <use xlinkHref="#github"></use>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <svg>
+                  <use xlinkHref="#twitter"></use>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <svg>
+                  <use xlinkHref="#instagram"></use>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <svg>
+                  <use xlinkHref="#facebook"></use>
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  </footer>
+);
 
 export default Footer
