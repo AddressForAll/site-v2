f3ae222b9fcd0471aebb3850b65cecebdabbd58a
elpbatista <elpbatista@gmail.com>
2022-03-23 11:56:54 -0700

    updating footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,67 +1,108 @@
-import React from 'react'
-import Container from '../Container/Container'
+import React from "react";
+import Container from "../Container/Container";
 
 const Footer = () => (
   <footer>
     <Container>
-      <div className="row">
-        <div className="col col-1-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          ducimus sunt sapiente dolorum doloremque voluptatibus delectus modi
-          aliquam quas dolor tempore nisi, est, quibusdam natus. Est illo
-          quisquam cumque mollitia?
+      <div class="row">
+        <div class="col col-1-5 align-items-center">
+          <div class="logo">
+            <svg>
+              <use xlink:href="#logosvg"></use>
+            </svg>
+          </div>
         </div>
-        <div className="col col-1-5">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
-          veritatis animi nemo provident iure culpa natus molestias est.
-          Temporibus consequuntur vero similique facere quam libero officiis
-          pariatur repudiandae tenetur nisi.
+        <div class="col col-1-5">
+          <ul class="nav" role="menu">
+            <li>
+              <a href="#">About US</a>
+            </li>
+            <li>
+              <a href="#">Statute</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Partnetships</a>
+            </li>
+          </ul>
+          <hr />
+          <ul class="nav" role="menu">
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+          </ul>
         </div>
-        <div className="col col-1-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-          praesentium, quae ea excepturi id libero, enim maiores, quo error
-          fugit ducimus! Corrupti ducimus qui labore dolorum voluptatum. Vitae,
-          sequi perferendis?
+        <div class="col col-1-5">
+          <ul class="nav" role="menu">
+            <li>
+              <a href="#">Data</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">API</a>
+            </li>
+            <li>
+              <a href="#">Tools</a>
+            </li>
+          </ul>
         </div>
-        <div className="col col-2-5">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga saepe
-          suscipit qui dolorum, blanditiis, mollitia repellat cupiditate
-          inventore, quasi ullam atque obcaecati modi odio perferendis
-          temporibus eos facilis? Qui, accusamus.
+        <div class="col col-2-5">
+          <h3>Contact Us</h3>
+          <address vocab="http://schema.org/" typeof="PostalAddress">
+            <span property="name" class="hidden">
+              Instituto AddressForAll
+            </span>
+            <span property="streetAddress">Av. Paulista, 171 4º andar</span>
+            <span property="addressLocality">Bela Vista</span>,
+            <span property="addressRegion">São Paulo - SP</span>
+            <span property="postalCode">01311-904</span>
+            <span property="addressCountry">Brasil</span>
+          </address>
+          <span itemprop="telephone">+55 11 4063-6401</span>
+          <a href="mailto:contato@addressforall.org" itemprop="email">
+            contato@addressforall.org
+          </a>
         </div>
       </div>
       <hr />
-      <div className="row">
-        <div className="col col-4-5">
+      <div class="row">
+        <div class="col col-4-5">
           <p>© 2022 AddressForAll</p>
         </div>
-        <div className="col col-1-5">
-          <ul id="sn-icons" className="nav">
+        <div class="col col-1-5">
+          <ul id="sn-icons" class="nav">
             <li>
               <a href="#">
                 <svg>
-                  <use xlinkHref="#github"></use>
+                  <use xlink:href="#github"></use>
                 </svg>
               </a>
             </li>
             <li>
               <a href="#">
                 <svg>
-                  <use xlinkHref="#twitter"></use>
+                  <use xlink:href="#twitter"></use>
                 </svg>
               </a>
             </li>
             <li>
               <a href="#">
                 <svg>
-                  <use xlinkHref="#instagram"></use>
+                  <use xlink:href="#instagram"></use>
                 </svg>
               </a>
             </li>
             <li>
               <a href="#">
                 <svg>
-                  <use xlinkHref="#facebook"></use>
+                  <use xlink:href="#facebook"></use>
                 </svg>
               </a>
             </li>
@@ -72,4 +113,4 @@ const Footer = () => (
   </footer>
 );
 
-export default Footer
+export default Footer;
