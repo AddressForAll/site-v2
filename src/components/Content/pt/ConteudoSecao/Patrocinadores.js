593a71d2e9a7772d2f7a1259d6383a3f3343e156
elpbatista <elpbatista@gmail.com>
2022-03-30 23:47:28 -0700

    fixing some errors

--- src/components/Content/pt/ConteudoSecao/Patrocinadores.js
+++ src/components/Content/pt/ConteudoSecao/Patrocinadores.js
@@ -4,31 +4,31 @@ import Container from "../../../Container/Container";
 
 const Patrocinadores = () => {
     return (
-    <Section id="sponsors">
-      <Container>
-        <h2>Sponsors</h2>
-        <p className="lead">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          illum mollitia esse quam ducimus delectus molestiae, natus repellendus
-          doloremque eligendi, neque minima quas, quaerat pariatur aut? Ab
-          laboriosam dolorem autem!
-        </p>
-        <ul className="sponsors flex-list">
-          <li>
-            <a
-              href="https://dadosabertos.social/c/dados-espaciais/12"
-              rel="external"
-            >
-              <img
-                src=""
-                alt="Dados abertos"
-              />
-            </a>
-          </li>
-        </ul>
-      </Container>
-    </Section>
-    )
+      <Section id="sponsors">
+        <Container>
+          <h2>Sponsors</h2>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            illum mollitia esse quam ducimus delectus molestiae, natus
+            repellendus doloremque eligendi, neque minima quas, quaerat pariatur
+            aut? Ab laboriosam dolorem autem!
+          </p>
+          <ul className="sponsors flex-list">
+            <li>
+              <a
+                href="https://dadosabertos.social/c/dados-espaciais/12"
+                rel="external"
+              >
+                <img
+                  src="http://addressforall.org/resources/img/dados_abertos.png"
+                  alt="Dados abertos"
+                />
+              </a>
+            </li>
+          </ul>
+        </Container>
+      </Section>
+    );
 }
 
 export default Patrocinadores;
