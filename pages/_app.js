2cc28302ad4c12b2b25910c3cce3cdaedb4cac1e
elpbatista <elpbatista@gmail.com>
2022-09-23 14:21:45 -0400

    adding Layout to every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
-// import Layout from "../components/layout";
 import "../styles/globals.scss";
-import { appWithTranslation } from 'next-i18next'
+import { appWithTranslation } from "next-i18next";
 
-// const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 function MyApp({ Component, pageProps }) {
-  return (
-    // <Layout>
-      <Component {...pageProps} />
-    // </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
-// export default MyApp;
 export default appWithTranslation(MyApp);
