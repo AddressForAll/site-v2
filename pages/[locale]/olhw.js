0aa25ec159d3411199e960f9162ebc37e9b72317
elpbatista <elpbatista@gmail.com>
2022-09-23 13:01:47 -0400

    OpenLayers Hello World

---
+++ pages/[locale]/olhw.js
@@ -0,0 +1,46 @@
+import { getStaticPaths, makeStaticProps } from "../../lib/getStatic";
+
+import React, { useState, useEffect, useRef } from "react";
+
+import Map from "ol/Map";
+import View from "ol/View";
+import XYZ from "ol/source/XYZ";
+import TileLayer from "ol/layer/Tile";
+import "ol/ol.css";
+
+export default function OpenLayersHelloWorld() {
+  const [map, setMap] = useState();
+  const mapElement = useRef();
+  const mapRef = useRef();
+  mapRef.current = map;
+
+  useEffect(() => {
+    const initialMap = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+          }),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+    setMap(initialMap);
+  }, []);
+
+  return (
+    <div
+      id="map"
+      style={{ height: "100vh", width: "100%" }}
+      ref={mapElement}
+      className="map-container"
+    />
+  );
+}
+
+const getStaticProps = makeStaticProps("common");
+export { getStaticPaths, getStaticProps };
